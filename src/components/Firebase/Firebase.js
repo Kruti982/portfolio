08f0640ae1c86b40888855c6d0085becc77ec800
Kruti982 <kruti.dimpi@gmail.com>
2025-04-08 16:55:29 +0530

    Update Firebase.js

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -4,7 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    //confgi
+     apiKey: "AIzaSyBEjgD3qfBJ3WfisAcMfthJ7DPXCZTPH8w",
+  authDomain: "portfolio-2bcee.firebaseapp.com",
+  projectId: "portfolio-2bcee",
+  storageBucket: "portfolio-2bcee.firebasestorage.app",
+  messagingSenderId: "204746487756",
+  appId: "1:204746487756:web:f635caabadfb53e5530f69",
+  measurementId: "G-M32388K03M"
+};
+
   };
 
   let firebase = initializeApp(firebaseConfig);
