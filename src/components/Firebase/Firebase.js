490fccd960c349f0549a554bb5198fd1c89eb0c6
rooney <abhishek@gmail.com>
2025-04-08 16:36:12 +0530

    added the routing code

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC_atfSVkK6n5YtKoNwUvVJfZH_K5BDxCQ",
-    authDomain: "tradingwithroy-ea2fd.firebaseapp.com",
-    projectId: "tradingwithroy-ea2fd",
-    // storageBucket: "tradingwithroy-ea2fd.appspot.com",
-    messagingSenderId: "166228950366",
-    appId: "1:166228950366:web:c1aeeb6f9f719c1dace3cf",
-    measurementId: "G-EKYXWXS4PR"
+    //confgi
   };
 
   let firebase = initializeApp(firebaseConfig);
