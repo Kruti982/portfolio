b89afa6b37a8e3bb4aa57b09e2ba5dabc1049e9a
Kruti982 <kruti.dimpi@gmail.com>
2025-04-08 17:12:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
 
 import Routing from './components/Routing/Routing'
-function App() {
-  return (
-    <Routing/>
-import Routing from "./components/routing/Routing";
 
 function App() {
   return (
